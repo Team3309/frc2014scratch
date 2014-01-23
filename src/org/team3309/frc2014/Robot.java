ff85707101acc8eb18b9e0d424f32a12215931e8
jon <jonathan.kavner@servitehs.org>
2014-01-22 19:17:35 -0800

    Added drive code

--- src/org/team3309/frc2014/Robot.java
+++ src/org/team3309/frc2014/Robot.java
@@ -7,7 +7,9 @@
 
 package org.team3309.frc2014;
 
-
+import edu.wpi.first.wpilibj.Compressor;
+import org.team3309.frc2014.drive.Drive;
+import org.team3309.friarlib.XboxController;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -24,16 +26,16 @@ import org.team3309.frc2014.commands.ExampleCommand;
  */
 public class Robot extends IterativeRobot {
 
-    Command autonomousCommand;
-
+    private Drive drive;
+    private XboxController driveXbox = new XboxController(1);
+    private XboxController operatorXbox = new XboxController(2);
+    private Compressor compressor = null;
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-        // instantiate the command used for the autonomous period
-        autonomousCommand = new ExampleCommand();
-
+        drive = new Drive();
         // Initialize all subsystems
         CommandBase.init();
     }
@@ -48,14 +50,10 @@ public class Robot extends IterativeRobot {
      */
     public void autonomousPeriodic() {
         Scheduler.getInstance().run();
+        
     }
 
     public void teleopInit() {
-	// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        autonomousCommand.cancel();
     }
 
     /**
@@ -63,6 +61,11 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
+        double rightX = driveXbox.getRightX();
+        double leftX = driveXbox.getLeftX();
+        double leftY = driveXbox.getLeftY();
+        drive.drive(leftY, rightX, leftX);
+        
     }
     
     /**
