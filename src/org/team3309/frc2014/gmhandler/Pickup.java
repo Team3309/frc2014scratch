157afaf97e05128f07f42bdb325339a3dd9cd4bb
Sang Woon (Ben) Lee <sangwoon.lee@servitehs.org>
2014-01-24 17:50:30 -0800

    - add gmhandler package - add Pickup and Launcher class - jon kavaner laptop sucks

---
+++ src/org/team3309/frc2014/gmhandler/Pickup.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.team3309.frc2014.gmhandler;
+
+/**
+ *
+ * @author Ben
+ */
+public class Pickup {
+    
+}
